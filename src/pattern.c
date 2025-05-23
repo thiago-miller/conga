d086f0278f6fbf95f8cdf0878acb86490e0bc3c4
Thiago L. A. Miller <thiago_leisrael@hotmail.com>
2025-05-23 17:52:48 -0300

    Add src/pattern.c (pattern_header_parse)

--- src/pattern.c
+++ src/pattern.c
@@ -6,18 +6,16 @@
 #include <error.h>
 #include "wrapper.h"
 
-#define STRSIZ 256
-
 typedef union
 {
 	int  num;
-	char str[STRSIZ];
-} PatternVal;
+	char str[BUFSIZ];
+} PatternHeaderVal;
 
-enum PatternTokenType
+enum PatternHeaderTokenType
 {
 	ROWS    = 258,
-	COLUNS  = 259,
+	COLS    = 259,
 	RULE    = 260,
 	EQUAL   = 261,
 	NUMBER  = 262,
@@ -27,15 +25,28 @@ enum PatternTokenType
 	MISTERY = 266
 };
 
+#define PATTERN_HEADER_NUM_OP 3
+
 static const char *const pattern_header_sym[] =
 {
-	"y", "x", "rule"
+	"y", "x", "rule", "=", NULL, NULL, ",", "\n", NULL
+};
+
+#define PATTERN_HEADER_TOKEN_SYM(t) \
+	(pattern_header_sym[(t) - ROWS])
+
+enum PatternHeaderParseState
+{
+	WAITING_KEY    = 258,
+	WAITING_EQUAL  = 259,
+	WAITING_VALUE  = 260,
+	WAITING_SEP    = 261
 };
 
 static const int
-pattern_header_sym_lookup (const char *key)
+pattern_header_op_lookup (const char *key)
 {
-	for (int i = 0; i < sizeof (pattern_header_sym) / sizeof (char *); i++)
+	for (int i = 0; i < PATTERN_HEADER_NUM_OP; i++)
 		{
 			if (!strncasecmp (key, pattern_header_sym[i],
 						strlen (pattern_header_sym[i])))
@@ -47,7 +58,7 @@ pattern_header_sym_lookup (const char *key)
 
 static int
 pattern_header_lex (const char *header, const char **pp,
-		PatternVal *val)
+		PatternHeaderVal *val)
 {
 	if (header != NULL)
 		*pp = header;
@@ -79,7 +90,7 @@ pattern_header_lex (const char *header, const char **pp,
 				}
 			else if (isalnum (c) || c == '/')
 				{
-					char key[STRSIZ] = {0};
+					char key[BUFSIZ] = {0};
 					int i = 0, token = 0;
 
 					do
@@ -87,20 +98,20 @@ pattern_header_lex (const char *header, const char **pp,
 						key[i++] = c;
 						c = *(*pp)++;
 					}
-					while (i < STRSIZ && (isalnum (c) || c == '/'));
+					while (i < BUFSIZ && (isalnum (c) || c == '/'));
 
-					key[STRSIZ - 1] = '\0';
+					key[BUFSIZ - 1] = '\0';
 
 					// ungetc
 					(*pp)--;
 
-					if ((token = pattern_header_sym_lookup (key)))
+					if ((token = pattern_header_op_lookup (key)))
 						return token;
 
 					if (val != NULL)
-						strncpy (val->str, key, STRSIZ);
+						strncpy (val->str, key, BUFSIZ);
 
-					val->str[STRSIZ - 1] = '\0';
+					val->str[BUFSIZ - 1] = '\0';
 
 					return STRING;
 				}
@@ -121,11 +132,194 @@ pattern_header_lex (const char *header, const char **pp,
 		}
 }
 
+static int
+pattern_header_parse (Pattern *pattern, const char *header)
+{
+	int mode = WAITING_KEY;
+	int seeneol = 0;
+
+	const char *p = NULL;
+	PatternHeaderVal val = {0};
+
+	int check[PATTERN_HEADER_NUM_OP] = {0};
+	int token_op = 0;
+
+	for (int token = pattern_header_lex (header, &p, &val); token;
+			token = pattern_header_lex (NULL, &p, &val))
+		{
+			if (seeneol)
+				break;
+
+			switch (token)
+				{
+				case ROWS : case COLS : case RULE :
+					{
+						switch (mode)
+							{
+							case WAITING_EQUAL: case WAITING_VALUE:
+								{
+									error (0, 0, "'%s' operator has no value",
+											PATTERN_HEADER_TOKEN_SYM (token_op));
+									return 0;
+								}
+							case WAITING_SEP:
+								{
+									error (0, 0, "Missing '%s' separator before '%s' operator",
+											PATTERN_HEADER_TOKEN_SYM (SEP),
+											PATTERN_HEADER_TOKEN_SYM (token));
+									return 0;
+								}
+							}
+
+						if (check[token - ROWS])
+							{
+								error (0, 0, "'%s' operator is repeated",
+										PATTERN_HEADER_TOKEN_SYM (token));
+								return 0;
+							}
+
+						check[token - ROWS] = 1;
+						token_op = token;
+						mode = WAITING_EQUAL;
+
+						break;
+					}
+				case EQUAL :
+					{
+						switch (mode)
+							{
+							case WAITING_KEY:
+								{
+									error (0, 0, "Missing key operator for '%s'",
+											PATTERN_HEADER_TOKEN_SYM (EQUAL));
+									return 0;
+								}
+							case WAITING_VALUE:
+								{
+									error (0, 0, "'%s' is repeated",
+											PATTERN_HEADER_TOKEN_SYM (EQUAL));
+									return 0;
+								}
+							case WAITING_SEP:
+								{
+									error (0, 0, "'%s' is misplaced",
+											PATTERN_HEADER_TOKEN_SYM (EQUAL));
+									return 0;
+								}
+							}
+
+						mode = WAITING_VALUE;
+
+						break;
+					}
+				case NUMBER : case STRING :
+					{
+						switch (mode)
+							{
+							case WAITING_KEY: case WAITING_SEP:
+								{
+									if (token == NUMBER)
+										error (0, 0, "Number '%d' with no operator",
+												val.num);
+									else if (token == STRING)
+										error (0, 0, "String '%s' with no operator",
+												val.str);
+									return 0;
+								}
+							case WAITING_EQUAL:
+								{
+									error (0, 0, "Missing '%s': Format key %s value",
+											PATTERN_HEADER_TOKEN_SYM (EQUAL),
+											PATTERN_HEADER_TOKEN_SYM (EQUAL));
+									return 0;
+								}
+							}
+
+						if (token == NUMBER && token_op == RULE)
+							{
+								error (0, 0, "'%s' requires a string",
+										PATTERN_HEADER_TOKEN_SYM (token_op));
+								return 0;
+							}
+						else if (token == STRING && (token_op == ROWS
+									|| token_op == COLS))
+							{
+								error (0, 0, "'%s' requires a number",
+										PATTERN_HEADER_TOKEN_SYM (token_op));
+								return 0;
+							}
+
+						switch (token_op)
+							{
+							case ROWS : pattern->rows = val.num;           break;
+							case COLS : pattern->cols = val.num;           break;
+							case RULE : pattern->rule = xstrdup (val.str); break;
+							}
+
+						mode = WAITING_SEP;
+
+						break;
+					}
+				case SEP :
+					{
+						switch (mode)
+							{
+							case WAITING_KEY : case WAITING_EQUAL :
+								{
+									error (0, 0, "'%s' separator is misplaced",
+											PATTERN_HEADER_TOKEN_SYM (SEP));
+									return 0;
+								}
+							case WAITING_VALUE :
+								{
+									error (0, 0, "Missing value for '%s' operator",
+											PATTERN_HEADER_TOKEN_SYM (token_op));
+									return 0;
+								}
+							}
+
+						mode = WAITING_KEY;
+
+						break;
+					}
+				case EOL :
+					{
+						seeneol = 1;
+						break;
+					}
+				case MISTERY :
+					{
+						error (0, 0, "What is that?");
+						return 0;
+					}
+				}
+		}
+
+	if (mode != WAITING_SEP)
+		{
+			error (0, 0, "Premature stop");
+			return 0;
+		}
+
+	int missing = 0;
+
+	for (int i = 0; i < PATTERN_HEADER_NUM_OP; i++)
+		{
+			if (!check[i] && (i + ROWS) != RULE)
+				{
+					error (0, 0, "Missing '%s' operator",
+							pattern_header_sym[i]);
+					missing = 1;
+				}
+		}
+
+	return !missing;
+}
+
 Pattern *
 pattern_new (const char *filename)
 {
 	Pattern *pattern = xcalloc (1, sizeof (Pattern));
-
 	return pattern;
 }
 
@@ -133,7 +327,9 @@ void
 pattern_free (Pattern *pattern)
 {
 	if (pattern == NULL)
-	return;
+		return;
+
+	xfree (pattern->rule);
 
 	xfree (pattern);
 }
--- tests/check_conga_pattern.c
+++ tests/check_conga_pattern.c
@@ -2,11 +2,67 @@
 
 #include "../src/pattern.c"
 
-#include <stdio.h>
+#define HEADER_SIZE(h) sizeof (h)/ sizeof (char *)
+
+static const char *header[] =
+{
+	"x = 10, y = 20, rule = B3/S23",
+	"x=30,y=10,RULE=S12345/B0123",
+	" x   = 5,   y =   5, rule  =  B3/S23",
+	"y = 10, x = 20, rule = B3/S23\n",
+	"  rule = B3/S12345, x = 10, y = 100\n",
+	"x = 10, y = 20"
+};
+
+static PatternHeaderVal val[][3] =
+{
+	{{.num = 10}, {.num = 20},  {.str = "B3/S23"}},
+	{{.num = 30}, {.num = 10},  {.str = "S12345/B0123"}},
+	{{.num = 5},  {.num = 5},   {.str = "B3/S23"}},
+	{{.num = 20}, {.num = 10},  {.str = "B3/S23"}},
+	{{.num = 10}, {.num = 100}, {.str = "B3/S12345"}},
+	{{.num = 10}, {.num = 20},  {}}
+};
+
+static const char *header_fail[] =
+{
+	"x 10, y = 20, rule = B3/S23",
+	"x = 10 y = 20, rule = B3/S23",
+	"x = 10, y 20, rule = B3/S23",
+	"x = 10, y = 20 rule = B3/S23",
+	"x = 10, y = 20, rule B3/S23",
+	" = 10, y = 20, rule = B3/S23",
+	"x = 10,  = 20, rule = B3/S23",
+	"x = 10, y = 20, rule B3/S23",
+	"x = , y = 20, rule = B3/S23",
+	"x = 10, y = , rule = B3/S23",
+	"x = 10, y = 20, rule = ",
+	", y = 20, rule = B3/S23",
+	"x = 10, , rule = B3/S23",
+	"x = 10, y = 20, ",
+	"x = 10, y = 20, rule = B3/S23,",
+	"y = 20, rule = B3/S23",
+	"x = 10, rule = B3/S23"
+};
+
+static Pattern *pattern = NULL;
+
+static void
+setup (void)
+{
+	pattern = pattern_new (NULL);
+}
+
+static void
+teardown (void)
+{
+	pattern_free (pattern);
+	pattern = NULL;
+}
 
 START_TEST (test_pattern_header_lex)
 {
-	PatternVal val = {0};
+	PatternHeaderVal val = {0};
 
 	const char header[] = "x = 10, Y = 20, ruLe = B3/S23\n+";
 	const char *p = NULL;
@@ -39,7 +95,7 @@ START_TEST (test_pattern_header_lex)
 						i += 2;
 						break;
 					}
-				case COLUNS:
+				case COLS:
 					{
 						ck_assert_int_eq (header[i], 'x');
 						i += 2;
@@ -78,6 +134,24 @@ START_TEST (test_pattern_header_lex)
 }
 END_TEST
 
+START_TEST (test_pattern_header_parse)
+{
+	ck_assert (pattern_header_parse (pattern, header[_i]));
+
+	ck_assert_int_eq (pattern->cols, val[_i][0].num);
+	ck_assert_int_eq (pattern->rows, val[_i][1].num);
+
+	if (pattern->rule != NULL)
+		ck_assert_str_eq (pattern->rule, val[_i][2].str);
+}
+END_TEST
+
+START_TEST (test_pattern_header_parse_fail)
+{
+	ck_assert (!pattern_header_parse (pattern, header_fail[_i]));
+}
+END_TEST
+
 Suite *
 make_pattern_suite (void)
 {
@@ -89,7 +163,12 @@ make_pattern_suite (void)
 	/* Core test case */
 	tc_core = tcase_create ("Core");
 
+	tcase_add_checked_fixture (tc_core, setup, teardown);
 	tcase_add_test (tc_core, test_pattern_header_lex);
+	tcase_add_loop_test (tc_core, test_pattern_header_parse,
+			0, HEADER_SIZE (header));
+	tcase_add_loop_test (tc_core, test_pattern_header_parse_fail,
+			0, HEADER_SIZE (header_fail));
 
 	suite_add_tcase (s, tc_core);
 
