4dfc6eac56d0993c672fcdfea60ede0eccad0aef
Thiago L. A. Miller <thiago_leisrael@hotmail.com>
2025-06-07 03:02:33 -0300

    Add src/conga.c: Key control of the window
    - Add movement with arrow keys
    - Add vim like shortcut as g, G, 0, $, o, O

--- src/conga.c
+++ src/conga.c
@@ -22,6 +22,13 @@ struct _Conga
 	Grid       *grid_next;
 	Rule       *rule;
 	Rand       *rng;
+	struct
+	{
+		int   done;
+		int   paused;
+		int   redraw;
+		int   resize;
+	} status;
 };
 
 static void
@@ -41,9 +48,12 @@ conga_set_game_from_pattern (Conga *game, const Config *cfg)
 		? cfg->cols
 		: pattern->grid->cols;
 
+	char *title = NULL;
+	asprintf (&title, "%s %s", cfg->progname, cfg->version);
+
 	*game = (Conga) {
 		.queue     = event_queue_new (FPS, cfg->delay),
-		.render    = render_new      ("ponga", rows, cols),
+		.render    = render_new      (title, rows, cols),
 		.grid_cur  = grid_new        (rows, cols),
 		.grid_next = grid_new        (rows, cols),
 		.rule      = rule_new        (rule),
@@ -52,15 +62,19 @@ conga_set_game_from_pattern (Conga *game, const Config *cfg)
 
 	cell_seed_from_grid (game->grid_cur, pattern->grid);
 
+	xfree (title);
 	pattern_free (pattern);
 }
 
 static void
 conga_set_random_game (Conga *game, const Config *cfg)
 {
+	char *title = NULL;
+	asprintf (&title, "%s %s", cfg->progname, cfg->version);
+
 	*game = (Conga) {
 		.queue     = event_queue_new (FPS, cfg->delay),
-		.render    = render_new      ("ponga", cfg->rows, cfg->cols),
+		.render    = render_new      (title, cfg->rows, cfg->cols),
 		.grid_cur  = grid_new        (cfg->rows, cfg->cols),
 		.grid_next = grid_new        (cfg->rows, cfg->cols),
 		.rule      = rule_new        (cfg->rule),
@@ -69,6 +83,8 @@ conga_set_random_game (Conga *game, const Config *cfg)
 
 	cell_seed_random_generation (game->grid_cur,
 			game->rng, cfg->live_percent);
+
+	xfree (title);
 }
 
 Conga *
@@ -103,29 +119,95 @@ conga_update_logic (Conga *game)
 }
 
 static inline void
-conga_update_graphics (Conga *game, int redraw)
+conga_update_graphics (Conga *game)
 {
-	if (redraw)
-		render_force_redraw (game->render);
+	if (game->status.resize)
+		render_force_resize (game->render);
 	render_draw (game->render, game->grid_cur);
 }
 
 static inline void
-conga_input_key (Conga *game, int key, int *done, int *paused)
+conga_input_key (Conga *game, int key)
 {
 	switch (key)
 		{
 		case 'q':
 		case 'Q':
 			{
-				*done = 1;
+				game->status.done = 1;
 				break;
 			}
 		case '\n':
 		case ' ' :
 			{
-				*paused = !(*paused);
-				event_queue_pause (game->queue, *paused);
+				game->status.paused = !(game->status.paused);
+				event_queue_pause (game->queue, game->status.paused);
+				break;
+			}
+		case KEY_UP:
+			{
+				render_scroll (game->render, game->grid_cur, -1, 0);
+				game->status.redraw = 1;
+				break;
+			}
+		case KEY_DOWN:
+			{
+				render_scroll (game->render, game->grid_cur, +1, 0);
+				game->status.redraw = 1;
+				break;
+			}
+		case KEY_LEFT:
+			{
+				render_scroll (game->render, game->grid_cur, 0, -1);
+				game->status.redraw = 1;
+				break;
+			}
+		case KEY_RIGHT:
+			{
+				render_scroll (game->render, game->grid_cur, 0, +1);
+				game->status.redraw = 1;
+				break;
+			}
+		case 'g':
+			{
+				render_scroll (game->render, game->grid_cur,
+						-1 * game->grid_cur->rows, 0);
+				game->status.redraw = 1;
+				break;
+			}
+		case 'G':
+			{
+				render_scroll (game->render, game->grid_cur,
+						game->grid_cur->rows, 0);
+				game->status.redraw = 1;
+				break;
+			}
+		case '$':
+			{
+				render_scroll (game->render, game->grid_cur,
+						0, game->grid_cur->cols);
+				game->status.redraw = 1;
+				break;
+			}
+		case '0':
+			{
+				render_scroll (game->render, game->grid_cur,
+						0, -1 * game->grid_cur->cols);
+				game->status.redraw = 1;
+				break;
+			}
+		case 'o':
+			{
+				render_scroll (game->render, game->grid_cur,
+						-1 * game->grid_cur->rows, -1 * game->grid_cur->cols);
+				game->status.redraw = 1;
+				break;
+			}
+		case 'O':
+			{
+				render_scroll (game->render, game->grid_cur,
+						game->grid_cur->rows, game->grid_cur->cols);
+				game->status.redraw = 1;
 				break;
 			}
 		}
@@ -135,14 +217,10 @@ void
 conga_run (Conga *game)
 {
 	Event event  = {0};
-	int   done   = 0;
-	int   paused = 0;
-	int   draw   = 0;
-	int   redraw = 0;
 
-	conga_update_graphics (game, 0);
+	conga_update_graphics (game);
 
-	while (!done)
+	while (!game->status.done)
 		{
 			event_queue_wait_for_event (game->queue, &event);
 
@@ -155,33 +233,33 @@ conga_run (Conga *game)
 					}
 				case EVENT_QUIT:
 					{
-						done = 1;
+						game->status.done = 1;
 						break;
 					}
 				case EVENT_KEY:
 					{
-						conga_input_key (game, event.key, &done, &paused);
+						conga_input_key (game, event.key);
 						break;
 					}
 				case EVENT_TIMER:
 					{
 						conga_update_logic (game);
-						draw = 1;
+						game->status.redraw = 1;
 						break;
 					}
 				case EVENT_WINCH:
 					{
-						draw   = 1;
-						redraw = 1;
+						game->status.redraw = 1;
+						game->status.resize = 1;
 						break;
 					}
 				}
 
-			if (draw)
+			if (game->status.redraw)
 				{
-					conga_update_graphics (game, redraw);
-					draw   = 0;
-					redraw = 0;
+					conga_update_graphics (game);
+					game->status.redraw = 0;
+					game->status.resize = 0;
 				}
 		}
 }
