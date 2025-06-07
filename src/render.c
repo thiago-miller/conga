d22b0d92c2183482aca9d3ea4095a8b253bd8a28
Thiago L. A. Miller <thiago_leisrael@hotmail.com>
2025-06-07 03:01:46 -0300

    Add src/render (render_scroll): Move win

--- src/render.c
+++ src/render.c
@@ -4,29 +4,60 @@
 #include <assert.h>
 #include "wrapper.h"
 
-#define BOX_FRAME_ROWS 2
-#define BOX_FRAME_COLS 2
+#define WIN_FRAME_ROWS 2
+#define WIN_FRAME_COLS 2
 
-#define BOX_TOTAL_ROWS(rows) ((rows) + BOX_FRAME_ROWS)
-#define BOX_TOTAL_COLS(cols) ((cols) * 2 + BOX_FRAME_COLS)
+#define WIN_TOTAL_ROWS(rows) ((rows) + WIN_FRAME_ROWS)
+#define WIN_TOTAL_COLS(cols) ((cols) * 2 + WIN_FRAME_COLS)
 
-#define BOX_GRID_ROWS(rows) ((rows) - BOX_FRAME_ROWS)
-#define BOX_GRID_COLS(cols) (((cols) - BOX_FRAME_COLS) / 2)
+#define WIN_GRID_ROWS(rows) ((rows) - WIN_FRAME_ROWS)
+#define WIN_GRID_COLS(cols) (((cols) - WIN_FRAME_COLS) / 2)
 
-#define BOX_START_ROW 1
-#define BOX_START_COL 1
+#define WIN_START_ROW 1
+#define WIN_START_COL 1
 
 struct _Render
 {
 	const char *title;
-	int         rows, cols;
+	int         view_row, view_col;
+	int         win_rows, win_cols;
 	int         cur_rows, cur_cols;
 	WINDOW     *outer_box;
 	WINDOW     *inner_box;
 };
 
+void
+render_scroll (Render *render, const Grid *grid, int dx, int dy)
+{
+	assert (render != NULL);
+	assert (grid != NULL);
+	assert (grid->rows >= render->cur_rows
+			&& grid->cols >= render->cur_cols);
+
+	render->view_row += dx;
+	render->view_col += dy;
+
+	if (render->view_row < 0)
+		render->view_row = 0;
+
+	if (render->view_col < 0)
+		render->view_col = 0;
+
+	int max_x = grid->rows - render->cur_rows;
+	int max_y = grid->cols - render->cur_cols;
+
+	if (render->view_row > max_x)
+		render->view_row = max_x;
+
+	if (render->view_col > max_y)
+		render->view_col = max_y;
+
+	mvwprintw (render->inner_box, 0, 2, "[%07d,%07d]",
+			render->view_row, render->view_col);
+}
+
 static inline void
-render_redraw (Render *render)
+render_resize_viewport (Render *render)
 {
 	int term_rows = 0;
 	int term_cols = 0;
@@ -41,19 +72,19 @@ render_redraw (Render *render)
 	box (render->outer_box, 0, 0);
 	mvwprintw (render->outer_box, 0, 2, "[%s]", render->title);
 
-	int inner_rows = term_rows - BOX_FRAME_ROWS * 2;
-	int inner_cols = term_cols - BOX_FRAME_COLS * 20;
-	int total_rows = BOX_TOTAL_ROWS (render->rows);
-	int total_cols = BOX_TOTAL_COLS (render->cols);
+	int inner_rows = term_rows - WIN_FRAME_ROWS * 2;
+	int inner_cols = term_cols - WIN_FRAME_COLS * 20;
+	int total_rows = WIN_TOTAL_ROWS (render->win_rows);
+	int total_cols = WIN_TOTAL_COLS (render->win_cols);
 
-	if (total_rows < BOX_FRAME_ROWS + 1 || total_rows > inner_rows)
+	if (total_rows < WIN_FRAME_ROWS + 1 || total_rows > inner_rows)
 		total_rows = inner_rows;
 
-	if (total_cols < BOX_FRAME_COLS + 1 || total_cols > inner_cols)
+	if (total_cols < WIN_FRAME_COLS + 1 || total_cols > inner_cols)
 		total_cols = inner_cols;
 
-	render->cur_rows = BOX_GRID_ROWS (total_rows);
-	render->cur_cols = BOX_GRID_COLS (total_cols);
+	render->cur_rows = WIN_GRID_ROWS (total_rows);
+	render->cur_cols = WIN_GRID_COLS (total_cols);
 
 	int inner_startx = 2;
 	int inner_starty = 2;
@@ -61,26 +92,27 @@ render_redraw (Render *render)
 	wresize (render->inner_box, total_rows, total_cols);
 	wclear (render->inner_box);
 	box (render->inner_box, 0, 0);
-	mvwprintw (render->inner_box, 0, 2, "[%d,%d]",
-			render->cur_rows, render->cur_cols);
+	mvwprintw (render->inner_box, 0, 2, "[%07d,%07d]",
+			render->view_row, render->view_col);
 
 	mvwin (render->inner_box, inner_starty, inner_startx);
 }
 
 void
-render_force_redraw (Render *render)
+render_force_resize (Render *render)
 {
 	assert (render != NULL);
 
-	// Force to get the terminal size
+	// Force ncurses to update terminal size info
+	// (workaround for SIGWINCH)
 	endwin();
 	refresh();
 
-	render_redraw (render);
+	render_resize_viewport (render);
 }
 
 Render *
-render_new (const char *title, int rows, int cols)
+render_new (const char *title, int win_rows, int win_cols)
 {
 	assert (title != NULL);
 
@@ -88,20 +120,20 @@ render_new (const char *title, int rows, int cols)
 	init_pair (1, COLOR_WHITE, COLOR_BLACK);
 	init_pair (2, COLOR_BLACK, COLOR_WHITE);
 
-	Render *render = xmalloc (sizeof (Render));
+	Render *render = xcalloc (1, sizeof (Render));
 
 	*render = (Render) {
 		.title     = xstrdup (title),
 		.outer_box = newwin (0, 0, 0, 0),
 		.inner_box = newwin (0, 0, 0, 0),
-		.rows      = rows,
-		.cols      = cols,
+		.win_rows  = win_rows,
+		.win_cols  = win_cols
 	};
 
 	wbkgd (render->outer_box, COLOR_PAIR (1));
 	wbkgd (render->inner_box, COLOR_PAIR (2));
 
-	render_redraw (render);
+	render_resize_viewport (render);
 
 	return render;
 }
@@ -120,27 +152,27 @@ render_free (Render *render)
 }
 
 void
-render_draw (Render *render, const Grid *grid_cur)
+render_draw (Render *render, const Grid *grid)
 {
 	assert (render != NULL);
-	assert (grid_cur != NULL);
+	assert (grid != NULL);
 
 	int cell = 0;
 
 	for (int i = 0; i < render->cur_rows; i++)
 		for (int j = 0; j < render->cur_cols; j++)
 			{
-				cell = GRID_GET (grid_cur, i, j);
+				cell = GRID_GET (grid, i + render->view_row, j + render->view_col);
 				wattron  (render->inner_box, COLOR_PAIR (-1 * cell + 2));
 
 				mvwaddch (render->inner_box,
-						i + BOX_START_ROW,
-						j * 2 + BOX_START_COL,
+						i + WIN_START_ROW,
+						j * 2 + WIN_START_COL,
 						' ');
 
 				mvwaddch (render->inner_box,
-						i + BOX_START_ROW,
-						j * 2 + BOX_START_COL + 1,
+						i + WIN_START_ROW,
+						j * 2 + WIN_START_COL + 1,
 						' ');
 
 				wattroff (render->inner_box, COLOR_PAIR (-1 * cell + 2));
--- src/render.h
+++ src/render.h
@@ -5,6 +5,7 @@
 typedef struct _Render Render;
 
 Render * render_new          (const char *title, int rows, int cols);
-void     render_draw         (Render *render, const Grid *grid_cur);
-void     render_force_redraw (Render *render);
+void     render_draw         (Render *render, const Grid *grid);
+void     render_scroll       (Render *render, const Grid *grid, int dx, int dy);
+void     render_force_resize (Render *render);
 void     render_free         (Render *render);
