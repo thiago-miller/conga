2740fba996e179c801dc210fb6b219d7f0c29de5
Thiago L. A. Miller <thiago_leisrael@hotmail.com>
2025-06-07 20:29:35 -0300

    Add src/cell (Cell): Keep cells alive and gen acm

--- src/cell.c
+++ src/cell.c
@@ -6,13 +6,13 @@
 #include "utils.h"
 
 void
-cell_seed_random_generation (Grid *grid, Rand *rng, float live_percent)
+cell_seed_random_generation (Grid *grid, Rand *rng, float live_percent, Cell *cell)
 {
 	assert (grid != NULL);
 	assert (live_percent > 0 && live_percent < 1);
 
 	int total_cells = grid->rows * grid->cols;
-	int live_cells = total_cells * live_percent;
+	int cells_alive = total_cells * live_percent;
 	int i = 0;
 
 	int *pos = xcalloc (total_cells, sizeof (int));
@@ -28,14 +28,17 @@ cell_seed_random_generation (Grid *grid, Rand *rng, float live_percent)
 
 	shuffle (pos, total_cells, rng);
 
-	for (i = 0; i < live_cells; i++)
+	for (i = 0; i < cells_alive; i++)
 		grid->data[pos[i]] = 1;
 
 	xfree (pos);
+
+	if (cell != NULL)
+		cell->alive = cells_alive;
 }
 
 void
-cell_seed_from_grid (Grid *grid_to, const Grid *grid_from)
+cell_seed_from_grid (Grid *grid_to, const Grid *grid_from, Cell *cell)
 {
 	assert (grid_to != NULL && grid_from != NULL);
 	assert (grid_to->rows >= grid_from->rows
@@ -43,21 +46,30 @@ cell_seed_from_grid (Grid *grid_to, const Grid *grid_from)
 
 	int offset_row = (grid_to->rows - grid_from->rows) / 2;
 	int offset_col = (grid_to->cols - grid_from->cols) / 2;
+	int cells_alive = 0;
+	int alive = 0;
 
 	for (int i = 0; i < grid_from->rows; i++)
 		for (int j = 0; j < grid_from->cols; j++)
-			GRID_SET (grid_to, offset_row + i, offset_col + j,
-					GRID_GET (grid_from, i, j));
+			{
+				alive = GRID_GET (grid_from, i, j);
+				GRID_SET (grid_to, offset_row + i, offset_col + j, alive);
+				cells_alive += alive;
+			}
+
+	if (cell != NULL)
+		cell->alive = cells_alive;
 }
 
 void
-cell_step_generation (Grid *grid_next, const Grid *grid_cur, Rule *rule)
+cell_step_generation (Grid *grid_next, const Grid *grid_cur, Rule *rule, Cell *cell)
 {
 	assert (grid_next != NULL && grid_cur != NULL);
 	assert (grid_next->rows == grid_cur->rows
 			&& grid_next->cols == grid_cur->cols);
 	assert (rule != NULL);
 
+	int cells_alive = 0;
 	int neighbors = 0;
 	int alive = 0;
 
@@ -70,6 +82,14 @@ cell_step_generation (Grid *grid_next, const Grid *grid_cur, Rule *rule)
 
 					GRID_SET (grid_next, i, j,
 							rule_next_state (rule, alive, neighbors));
+
+					cells_alive += alive;
 				}
+		}
+
+	if (cell != NULL)
+		{
+			cell->alive = cells_alive;
+			cell->gen += 1;
 		}
 }
--- src/cell.h
+++ src/cell.h
@@ -4,6 +4,12 @@
 #include "rule.h"
 #include "rand.h"
 
-void cell_seed_random_generation (Grid *grid, Rand *rng, float live_percent);
-void cell_seed_from_grid         (Grid *grid_to, const Grid *grid_from);
-void cell_step_generation        (Grid *grid_next, const Grid *grid_cur, Rule *rule);
+typedef struct
+{
+	unsigned int alive;
+	unsigned int gen;
+} Cell;
+
+void cell_seed_random_generation (Grid *grid, Rand *rng, float live_percent, Cell *cell);
+void cell_seed_from_grid         (Grid *grid_to, const Grid *grid_from, Cell *cell);
+void cell_step_generation        (Grid *grid_next, const Grid *grid_cur, Rule *rule, Cell *cell);
